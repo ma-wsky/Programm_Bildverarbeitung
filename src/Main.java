472239e2b865d20c49d342c3c565195d018e1681
ma-wsky <maximilian.dregewsky@gmail.com>
2026-06-02 17:49:56 +0200

    Rewrite class ImageManipulation with static methods. make image output folder /generated

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,18 @@ import java.awt.image.BufferedImage;
 
 void main() {
     ImageIO io = new ImageIO();
-    BufferedImage input = io.readImageToPPM("pics/obama.jpg", "obama.ppm");
-    ColorManipulation colorManipulation = new ColorManipulation();
-    colorManipulation.grayScale(input);
-    io.displayImage(input);
+    BufferedImage input = io.readImageAndConvertToPPM("pics/obama.jpg", "generated/obama.ppm");
+    BufferedImage grayScaleImage = ColorManipulation.grayScale(input);
+    ImageIO.displayImage(input);
+    ImageIO.displayImage(grayScaleImage);
 
-    ImageManipulation manipulation = new ImageManipulation(input);
-    //manipulation.equidensityFirstOrderGrayImage();
-    //manipulation.equidensitySecondOrderGrayImage();
-
-    //manipulation.histogramEqualization(input);
-    manipulation.gammaCorrection(0.3, input);
-
-    BufferedImage gamma = io.readPPM(new File("gammaCorrected.ppm"));
-    DescriptiveStatistics stats = new DescriptiveStatistics(gamma);
+    DescriptiveStatistics stats = new DescriptiveStatistics(input);
     stats.calculateAllStatistics();
     stats.printStatistics();
+
+    ImageIO.displayImage(ImageManipulation.linearScaleGrayImage(input, 0, 4));
+    ImageIO.displayImage(ImageManipulation.equidensityFirstOrderGrayImage(input));
+    ImageIO.displayImage(ImageManipulation.equidensitySecondOrderGrayImage(input));
+    ImageIO.displayImage(ImageManipulation.histogramEqualization(input));
+    ImageIO.displayImage(ImageManipulation.gammaCorrection(input, 3));
 }
